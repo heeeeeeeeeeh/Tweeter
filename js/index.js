6977cd49788f2e591befa8be561b28881f512924
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-02-14 06:28:17 -0600

    fix: set remaining characters when tweet goes over limit

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ function updateCharRem(e) {
   let charRem = document.getElementById("charRemainingNum")
   if (tweetText.length > 160) {
     e.target.value = tweetText.substring(0, 160);
+    charRem.innerHTML = 0
   } else {
     charRem.innerHTML = (160 - tweetText.length)
   }
