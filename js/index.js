1a5a6e6bd346eaed2c1b87959442bb7cafae1e88
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-02-13 19:46:02 -0600

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  let tweet = document.getElementById("tweetInput")
+
+  tweet.oninput = updateCharRem
+}
+
+function updateCharRem(e) {
+  let tweetText = e.target.value;
+  let charRem = document.getElementById("charRemainingNum")
+  if (tweetText.length > 160) {
+    e.target.value = tweetText.substring(0, 160);
+  } else {
+    charRem.innerHTML = (160 - tweetText.length)
+  }
+}
